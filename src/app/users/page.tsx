8edf07f2fb686524bab27aff43492e25708a8981
Zyad Saber <zyad.2002.best@gmail.com>
2024-07-24 18:36:34 +0300

    update on lock file

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,20 +1,13 @@
 'use client'
 
 import { memo } from "react";
-import createNotification from "@/components/notification";
 
 const UsersPage = () => {
-
-
-
-    const handle = () => {
-        createNotification()
-    }
 
     return (
         <>
             <h1>users page</h1>
-            <button onClick={handle}>click me</button>
+            {/* <button onClick={handle}>click me</button> */}
         </>
     )
 }
