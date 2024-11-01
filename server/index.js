caf6c70d31e4441e5f4c2791784d4fc2ec67025b
Zyad Saber <zyad.2002.best@gmail.com>
2024-11-01 11:34:43 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 5000;
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(logger);
+// app.use(express.urlencoded({ extends: false }));
+app.use(express.json());
+
+const whiteList = [
+  process.env.domain,
+  "http://127.0.0.1:5000/",
+  "http://localhost:5000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
 
-// All other routes should serve the React app
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
